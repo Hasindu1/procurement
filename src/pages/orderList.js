5254d3f7d4e74fd6090e40c8d7bf0d0e86e3300f
plhewa <plhewa2@gmail.com>
2020-09-27 11:18:53 +0530

    Order List Interface Creation Part 1

--- src/pages/orderList.js
+++ src/pages/orderList.js
@@ -5,9 +5,40 @@ export default function OrderList() {
     return (
         <>
         <NavBar/>
-       <div>
-           This is the OrderList
-       </div>
+        <ul className="list-group my-5">
+                <h2 className="text-capitalize text-center">Requests</h2>
+                
+                <li className="list-group-item text-capitalize d-flex justify-content-between my-2" >
+                 <div><h6>Job Reference No : A2r5345435345</h6></div> 
+                 <div> <h6>Total      : 1,150,000LKR</h6> </div>
+                
+               
+                            <div className="todo-icon">
+                             
+                            </div>
+                 </li>
+                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                 <h6>Job Reference No : A2r5345435345</h6> 
+                <h6>Total      : 1,150,000LKR</h6> 
+                            <div className="todo-icon">
+                            <span className="mx-2 text-primary"><i className="fas fa-envelope"></i><i className="fas fa-share fa-xs"></i></span>
+                            </div>
+                 </li>
+                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                 <h6>Job Reference No : A2r5345435345</h6> 
+                <h6>Total      : 1,150,000LKR</h6> 
+                            <div className="todo-icon">
+                             
+                            </div>
+                 </li>
+                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                 <h6>Job Reference No : A2r5345435345</h6> 
+                <h6>Total      : 1,150,000LKR</h6> 
+                            <div className="todo-icon">
+                             <span className="mx-2 text-primary"><i className="fas fa-envelope"></i><i className="fas fa-share fa-xs"></i></span>
+                            </div>
+                 </li>
+            </ul>
        </>
     )
 }
