34f95a2779848a9b527b6cd4fd05db518370926b
plhewa <plhewa2@gmail.com>
2020-09-27 17:30:26 +0530

    Fetch Orders to the Order list page

--- src/pages/orderList.js
+++ src/pages/orderList.js
@@ -1,44 +1,56 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import NavBar from '../components/navbar'
+import firebase from '../Firebase'
 
-export default function OrderList() {
+
+function useOrders(){
+    const [orders,setOrders] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = firebase
+            .firestore()
+            .collection('orders')
+            .onSnapshot((snapshot) => {
+                const newOrders = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+
+                setOrders(newOrders)
+            })
+
+            return () => unsubscribe()
+    },[])
+
+    return orders
+}
+
+
+const OrderList = () =>{
+
+
+    const orders = useOrders();
+
     return (
         <>
         <NavBar/>
         <ul className="list-group my-5">
                 <h2 className="text-capitalize text-center">Requests</h2>
                 
-                <li className="list-group-item text-capitalize d-flex justify-content-between my-2" >
-                 <div><h6>Job Reference No : A2r5345435345</h6></div> 
-                 <div> <h6>Total      : 1,150,000LKR</h6> </div>
-                
-               
-                            <div className="todo-icon">
-                             
-                            </div>
-                 </li>
-                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-                 <h6>Job Reference No : A2r5345435345</h6> 
-                <h6>Total      : 1,150,000LKR</h6> 
-                            <div className="todo-icon">
-                            <span className="mx-2 text-primary"><i className="fas fa-envelope"></i><i className="fas fa-share fa-xs"></i></span>
-                            </div>
-                 </li>
-                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-                 <h6>Job Reference No : A2r5345435345</h6> 
-                <h6>Total      : 1,150,000LKR</h6> 
-                            <div className="todo-icon">
-                             
-                            </div>
-                 </li>
-                 <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-                 <h6>Job Reference No : A2r5345435345</h6> 
-                <h6>Total      : 1,150,000LKR</h6> 
-                            <div className="todo-icon">
-                             <span className="mx-2 text-primary"><i className="fas fa-envelope"></i><i className="fas fa-share fa-xs"></i></span>
-                            </div>
-                 </li>
+                {orders.map((order) => 
+
+                        <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                            <h6>Job Reference No : {order.id}</h6> 
+                            <h6>Total      : {order.total}</h6> 
+                                <div className="todo-icon">
+                                    <span className="mx-2 text-primary"><i className="fas fa-envelope"></i><i className="fas fa-share fa-xs"></i></span>
+                                </div>
+                        </li>
+
+                )}
             </ul>
        </>
     )
 }
+
+export default OrderList
